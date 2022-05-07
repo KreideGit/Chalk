ad174813c49976595a2c6c175ca23a883e3f28d6
Christian Kreidenhuber <kreidenhuber.c@gmail.com>
2022-05-07 16:18:29 +0200

    [FEATURE] setup demo application

--- Blackboard/src/main.cpp
+++ Blackboard/src/main.cpp
@@ -1,7 +1,17 @@
-#include <Chalk.h>
+#include <Core/Application.h>
 
 int main(int argc, char** argv)
 {
-	SayHi();
+	Chalk::Settings settings
+	{
+		.ApplicationName = "Blackboard App",
+		.WindowName = "Blackboard",
+		.WindowWidth = 2560,
+		.WindowHeight = 1440
+	};
+
+	Chalk::Application app(settings);
+	app.Start();
+
 	return 0;
 }
